565ec5482980978d47092a0cae765986541d924c
Brian Han <bmhan319@hotmail.com>
2020-11-02 23:31:19 -0800

    styling input placeholder text

--- src/Component/IpInput.js
+++ src/Component/IpInput.js
@@ -4,7 +4,7 @@ export default function IpInput() {
   return (
     <div>
       <form className="ip-form">
-        <input className="ip-input" placeholder="IP Address" type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" />
+        <input className="ip-input" placeholder="Search for any IP address" type="text" minlength="7" maxlength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" />
         <button className="ip-submit" type="submit"></button>
       </form>
     </div>
