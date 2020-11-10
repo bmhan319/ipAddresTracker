c3a278d5ac5da239cb92a84af5df1056608fac8d
Brian Han <bmhan319@hotmail.com>
2020-11-09 22:10:37 -0800

    cleaned up styling on details box, moved map zoom control to bottom right corner

--- src/Component/LeafMap.js
+++ src/Component/LeafMap.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet";
 
 class LeafMap extends Component {
   render() {
     let position = [this.props.state.lat, this.props.state.long]
     return (
       <div key={this.props.state.counter} className="mapContainer">
-        <MapContainer id="myMap" center={position} zoom={this.props.state.zoom} scrollWheelZoom={false}>
+        <MapContainer id="myMap" 
+                      center={position} 
+                      zoom={this.props.state.zoom} 
+                      zoomControl={false}
+                      minZoom={3}
+                      maxZoom={18}
+                      scrollWheelZoom={false}>
+          <ZoomControl position="bottomright" />
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
