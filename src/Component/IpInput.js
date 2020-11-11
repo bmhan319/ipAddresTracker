ac6d6e6fbcc39702ee6c1165cf338ec2f4ec2b20
Brian Han <bmhan319@hotmail.com>
2020-11-10 21:05:00 -0800

    added form attributes

--- src/Component/IpInput.js
+++ src/Component/IpInput.js
@@ -4,8 +4,8 @@ export default function IpInput(props) {
   return (
     <div>
       <form className="ip-form" onSubmit={props.handleSubmit}>
-        <input className="ip-input" onChange={props.handleInput} placeholder="Search for any IP address" type="text" minLength="7" maxLength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" />
-        <button className="ip-submit" type="submit"></button>
+        <input className="ip-input" title="IP Input" onChange={props.handleInput} placeholder="Search for any IP address" type="text" minLength="7" maxLength="15" size="15" pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" />
+        <button className="ip-submit" title="submit" type="submit"></button>
       </form>
     </div>
   )
